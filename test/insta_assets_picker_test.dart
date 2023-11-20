c473da99f5e6d89adea72ecf2b7cfe71a4177eb6
Maël Le Goff <legoffmael@gmail.com>
2023-11-20 19:23:56 +0900

    ✅ add basic test

--- test/insta_assets_picker_test.dart
+++ test/insta_assets_picker_test.dart
@@ -1,1 +1,23 @@
-void main() {}
+import 'package:flutter_test/flutter_test.dart';
+import 'package:insta_assets_picker/insta_assets_picker.dart';
+import 'package:insta_assets_picker/src/insta_assets_crop_controller.dart';
+
+void main() {
+  test('Ensure nextCropRatio() loop', () {
+    final InstaAssetsCropController controller =
+        InstaAssetsCropController(false, const InstaAssetCropDelegate());
+
+    expect(controller.aspectRatio, 1);
+    expect(controller.aspectRatioString, '1:1');
+
+    controller.nextCropRatio();
+
+    expect(controller.aspectRatio, 4 / 5);
+    expect(controller.aspectRatioString, '4:5');
+
+    controller.nextCropRatio();
+
+    expect(controller.aspectRatio, 1);
+    expect(controller.aspectRatioString, '1:1');
+  });
+}
